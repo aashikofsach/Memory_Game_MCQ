4e6f5967fe5d59aa1c8c254647fbc1ef38dae1fc
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-08-26 18:17:05 +0530

    make function to handle gridInput Value

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 function MemoryGame() {
 
-    const [grid , setGrid] = useState(4);
+    const [gridSize , setGridSize] = useState();
     const [cards , setCards] = useState([]);
 
     const [flipped , setFlipped] = useState([]);
@@ -10,10 +10,24 @@ function MemoryGame() {
 
     const [won, setWon] = useState();
 
+    function handleGridChangeSize(e)
+    {
+      // console.log("jai ")
+      // console.log(typeof(e.target.value))
+      let size = parseInt(e.target.value);
+      if(size >= 2 && size <=10)
+        setGridSize(size)
+    
+    }
+
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4'>
         <h1 className='text-3xl font-bold mb-6'>Memory Game</h1>
         {/* Input */}
+        <div className='mb-4'>
+          <label htmlFor="gridSize" className='mr-2'>Grid Size : (max 10)</label>
+          <input id='gridSize' type="number" min={2} max={10} value={gridSize} onChange={(e)=>handleGridChangeSize(e)} className='border-2 border-gray-300 px-2 py-1 rounded-xl' />
+        </div>
     </div>
   )
 }
