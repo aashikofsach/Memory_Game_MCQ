81c538d35ea5c97a7e24aea84accbc00cc528153
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-08-27 17:45:47 +0530

    so adjust the required number for odd input, and also make the input editable

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function MemoryGame() {
 
@@ -15,19 +15,30 @@ function MemoryGame() {
     {
       // console.log("jai ")
       // console.log(typeof(e.target.value))
-      let size = parseInt(e.target.value);
-      if(size >= 2 && size <=10)
+      let size = parseInt(e.target.value) || "";
+     
         setGridSize(size)
     
     }
 
     function InitializeGame()
     {
+        if (gridSize < 2 || gridSize > 10) return; // safety check
       const totalCards = gridSize*gridSize;
       const pairCounts = Math.floor(totalCards/2);
-      const numbers = [...Array(pairCounts).keys()].map((_ , index) => index +1)
-      // console.log(numbers);
-      const shuffleCards = [...numbers, ...numbers].sort(()=> Math.random()-0.5).map((number , index)=> ({id: index , number}))
+     // make pairs
+  const numbers = [...Array(pairCounts).keys()].map((_, index) => index + 1);
+  let shuffleCards = [...numbers, ...numbers];
+
+  // if odd, add one extra "lonely" card
+  if (totalCards % 2 !== 0) {
+    shuffleCards.push(0); // 0 or any special marker
+  }
+
+  // shuffle & assign ids
+  shuffleCards = shuffleCards
+    .sort(() => Math.random() - 0.5)
+    .map((number, index) => ({ id: index, number }));
       console.log(shuffleCards)
       setCards(shuffleCards);
       setFlipped([]);
@@ -35,15 +46,27 @@ function MemoryGame() {
       setWon(false)
     }
 
-    InitializeGame() ;
-
+   
+    useEffect(() => {
+    
+      InitializeGame();
+     
+      
+    }, [gridSize])
+    
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4'>
         <h1 className='text-3xl font-bold mb-6'>Memory Game</h1>
         {/* Input */}
         <div className='mb-4'>
           <label htmlFor="gridSize" className='mr-2'>Grid Size : (max 10)</label>
-          <input id='gridSize' type="number" min={2} max={10} value={gridSize} onChange={(e)=>handleGridChangeSize(e)} className='border-2 border-gray-300 px-2 py-1 rounded-xl' />
+          <input id='gridSize' type="number" min={2} max={10} value={gridSize} onChange={handleGridChangeSize} className='border-2 border-gray-300 px-2 py-1 rounded-xl' />
+        </div>
+        {/* gameboard */}
+        <div className={`grid gap-2`} style={{gridTemplateColumns : `repeat(${gridSize} , 1fr)` }}>
+          {
+            cards.map((card)=> <div key={card.id}>{card.number}</div>)
+          }
         </div>
     </div>
   )
