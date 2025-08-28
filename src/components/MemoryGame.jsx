edef200b84f325ec9c8974a4d5ddef89c81d3557
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-08-28 19:17:38 +0530

    writing the complex logic for isFlipped and isSolved to decide color... nice one very important and good exercise

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -2,90 +2,117 @@ import React, { useEffect, useState } from 'react'
 
 function MemoryGame() {
 
-    const [gridSize , setGridSize] = useState(4);
-    const [cards , setCards] = useState([]);
+  const [gridSize, setGridSize] = useState(4);
+  const [cards, setCards] = useState([]);
 
-    const [flipped , setFlipped] = useState([]);
-    const [solved , setSolved] = useState([])
-    const [disabled , setDisabled] = useState([]);
+  const [flipped, setFlipped] = useState([]);
+  const [solved, setSolved] = useState([])
+  const [disabled, setDisabled] = useState([]);
 
-    const [won, setWon] = useState();
+  const [won, setWon] = useState();
 
-    function handleGridChangeSize(e)
-    {
-      // console.log("jai ")
-      // console.log(typeof(e.target.value))
-      let size = parseInt(e.target.value) || "";
-     
-        setGridSize(size)
-    
-    }
+  function handleGridChangeSize(e) {
+    // console.log("jai ")
+    // console.log(typeof(e.target.value))
+    let size = parseInt(e.target.value) || "";
 
-    function InitializeGame()
-    {
-        if (gridSize < 2 || gridSize > 10) return; // safety check
-      const totalCards = gridSize*gridSize;
-      const pairCounts = Math.floor(totalCards/2);
-     // make pairs
-  const numbers = [...Array(pairCounts).keys()].map((_, index) => index + 1);
-  let shuffleCards = [...numbers, ...numbers];
+    setGridSize(size)
 
-  // if odd, add one extra "lonely" card
-  if (totalCards % 2 !== 0) {
-    shuffleCards.push(0); // 0 or any special marker
   }
 
-  // shuffle & assign ids
-  shuffleCards = shuffleCards
-    .sort(() => Math.random() - 0.5)
-    .map((number, index) => ({ id: index, number }));
-      console.log(shuffleCards)
-      setCards(shuffleCards);
-      setFlipped([]);
-      setSolved([]);
-      setWon(false)
+  function InitializeGame() {
+    if (gridSize < 2 || gridSize > 10) return; // safety check
+    const totalCards = gridSize * gridSize;
+    const pairCounts = Math.floor(totalCards / 2);
+    // make pairs
+    const numbers = [...Array(pairCounts).keys()].map((_, index) => index + 1);
+    let shuffleCards = [...numbers, ...numbers];
+
+    // if odd, add one extra "lonely" card
+    if (totalCards % 2 !== 0) {
+      shuffleCards.push(0); // 0 or any special marker
     }
 
-   
-    useEffect(() => {
-    
-      InitializeGame();
-     
-      
-    }, [gridSize])
-    
+    // shuffle & assign ids
+    shuffleCards = shuffleCards
+      .sort(() => Math.random() - 0.5)
+      .map((number, index) => ({ id: index, number }));
+    console.log(shuffleCards)
+    setCards(shuffleCards);
+    setFlipped([]);
+    setSolved([]);
+    setWon(false)
+  }
 
-    function handleClick(id)
+
+  useEffect(() => {
+
+    InitializeGame();
+
+
+  }, [gridSize]);
+
+  function checkMatch(secondId)
+  {
+    let [firstId] = flipped;
+    if(cards[firstId].number=== cards[secondId].number)
     {
+      setSolved([...solved,firstId, secondId])
+      setFlipped([])
+      console.log("matched",solved)
+    }
+    else{
+      setTimeout(()=>{
+        setFlipped([])
+      },1000)
+    }
 
-      if(flipped.length===0)
-      {
-        setFlipped([id])
-        return ;
+  }
+
+
+  function handleClick(id) {
+
+    if (flipped.length === 0) {
+      setFlipped([id])
+      return;
+    }
+    if (flipped.length === 1) {
+      // setDisabled(true)
+      if (id !== flipped[0]) {
+        setFlipped([...flipped, id])
+        //check match or not 
+        checkMatch(id)
       }
-
+      else {
+        setFlipped([]);
+        // setDisabled(false)
+      }
     }
 
-    const isFlipped = (id) => {
+  }
 
-      return flipped.includes(id)
+  const isFlipped = (id) => {
 
-    }
+    return flipped.includes(id) || solved.includes(id)
+
+  }
+
+  const isSolved =(id) => solved.includes(id);
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4'>
-        <h1 className='text-3xl font-bold mb-6'>Memory Game</h1>
-        {/* Input */}
-        <div className='mb-4'>
-          <label htmlFor="gridSize" className='mr-2'>Grid Size : (max 10)</label>
-          <input id='gridSize' type="number" min={2} max={10} value={gridSize} onChange={handleGridChangeSize} className='border-2 border-gray-300 px-2 py-1 rounded-xl' />
-        </div>
-        {/* gameboard */}
-        <div className={`grid gap-2 mb-4 `} style={{gridTemplateColumns : `repeat(${gridSize} , 1fr)`, width : `${gridSize*100}px` }}>
-          {
-            cards.map((card)=> <div className={`aspect-square flex items-center justify-center text-xl font-bold cursor-pointer rounded-lg  text-gray-400 transition-all duration-300 ${isFlipped(card.id) ? `bg-blue-500 text-white` : `bg-gray-300 text-gray-400`}`} key={card.id} onClick={()=> handleClick(card.id)}>{isFlipped(card.id) ? card.number : "?"}</div>)
-          }
-        </div>
+      <h1 className='text-3xl font-bold mb-6'>Memory Game</h1>
+      {/* Input */}
+      <div className='mb-4'>
+        <label htmlFor="gridSize" className='mr-2'>Grid Size : (max 10)</label>
+        <input id='gridSize' type="number" min={2} max={10} value={gridSize} onChange={handleGridChangeSize} className='border-2 border-gray-300 px-2 py-1 rounded-xl' />
+      </div>
+      {/* gameboard */}
+      <div className={`grid gap-2 mb-4 `} style={{ gridTemplateColumns: `repeat(${gridSize} , 1fr)`, width: `${gridSize * 100}px` }}>
+        {
+          cards.map((card) => <div className={`aspect-square flex items-center justify-center text-xl font-bold cursor-pointer rounded-lg  text-gray-400 transition-all duration-300 ${isFlipped(card.id) ? isSolved(card.id) ? `bg-green-500 text-white`: `bg-blue-500 text-white` : `bg-gray-300 text-gray-400`}`} key={card.id} onClick={() => handleClick(card.id)}>{isFlipped(card.id) ? card.number : "?"}</div>)
+        }
+      </div>
     </div>
   )
 }
